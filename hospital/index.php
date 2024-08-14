e02df1af3bd92d9615852f5ec4b7c4ac7af13208
Mr - A N A S <mr.anas1003@gmail.com>
2024-08-14 15:43:54 +0500

    change ./hospitalDashboard.php to ./index.php

--- hospital/index.php
+++ hospital/index.php
@@ -14,7 +14,7 @@
         <div class="sidebar">
             <div class="h4 mb-4 px-3">Hospital Name</div>
             <ul class="list-unstyled">
-                <li><a href="hospitalDashboard.php">Dashboard</a></li>
+                <li><a href="./index.php">Dashboard</a></li>
                 <li><a href="./views/patientsList.php">Patient Requests</a></li>
                 <li><a href="./views/testResults.php">Test Results</a></li>
                 <li><a href="./views/vaccination.php">Vaccination Status</a></li>
